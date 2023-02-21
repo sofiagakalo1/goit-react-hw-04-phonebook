da032adee3f1b423cda30ae30fd91ce87ef4c5b0
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-02-21 16:23:39 +0100

    contactsform

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -49,7 +49,7 @@ const ContactsForm = ({ onSubmit }) => {
             type="tel"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            title="Phone numberr must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
         </div>
